40c318981712231ab4a7ee33168a6549fa608900
Laura <lauraliugailaite@gmail.com>
2021-04-26 23:07:33 +0200

    burger

---
+++ js/main.js
@@ -0,0 +1,16 @@
+"use strict"
+
+let burger = document.querySelector('.burger');
+
+let navigation = document.querySelector('.nav');
+
+burger.addEventListener('click', () => {
+    
+   if (navigation.className == 'nav on') {
+       navigation.classList.remove('on');
+   } else {
+       navigation.classList.add('on');
+   }
+
+});
+
